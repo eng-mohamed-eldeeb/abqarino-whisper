8b39bd98a531cf2f55ddef9cdbab29c4abca3ad8
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-06-25 16:08:51 +0300

    Add whisper -static

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import whisper
+import sys
 import os
 from flask import Flask
 
 app = Flask(__name__)
+model = whisper.load_model("small")
 env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
 @app.route("/")
 def index():
- return "Hello World!"
+    model = whisper.load_model("small")
+    result = model.transcribe("audio.mp3")
+    return ( result["text"].encode('utf-8').decode(sys.stdout.encoding))
