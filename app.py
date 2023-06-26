1765dbf53e999e20fbcec92091104a15a11e25cf
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-06-26 19:44:56 +0300

    Change the model to tiny #2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-model = whisper.load_model("tiny")
+model = whisper.load_model("small")
 env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
