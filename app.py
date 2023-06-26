817bcb27288ff7f9934ef93d2636bd5eba28224f
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-06-26 20:14:37 +0300

    Another test

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-import whisper
-import sys
+# import whisper
+# import sys
 import os
 from flask import Flask
 import json
 
 app = Flask(__name__)
-model = whisper.load_model("small")
+# model = whisper.load_model("small")
 env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    model = whisper.load_model("tiny")
-    result = model.transcribe("audio.mp3")
-    return json.dumps({"text": result["text"].encode('utf-8').decode(sys.stdout.encoding)})
+    # model = whisper.load_model("tiny")
+    # result = model.transcribe("audio.mp3")
+    # return json.dumps({"text": result["text"].encode('utf-8').decode(sys.stdout.encoding)})
+    return json.dumps({"text": "Hello World!"})
